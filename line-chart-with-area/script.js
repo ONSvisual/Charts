832f596125c52c540d5a3132b554b8ae23888a71
Micheal Slade <50627681+Micheal-Slade@users.noreply.github.com>
2023-07-05 11:15:20 +0100

    initial commit

---
+++ line-chart-with-area/script.js
@@ -0,0 +1,292 @@
+let graphic = d3.select('#graphic');
+//console.log(`Graphic selected: ${graphic}`);
+
+let pymChild = null;
+
+function drawGraphic() {
+	//Accessible summary
+	d3.select('#accessibleSummary').html(config.essential.accessibleSummary);
+	//	console.log(`Accessible summary set: ${config.essential.accessibleSummary}`);
+
+	let threshold_md = config.optional.mediumBreakpoint;
+	let threshold_sm = config.optional.mobileBreakpoint;
+
+	//set variables for chart dimensions dependent on width of #graphic
+	if (parseInt(graphic.style('width')) < threshold_sm) {
+		size = 'sm';
+	} else if (parseInt(graphic.style('width')) < threshold_md) {
+		size = 'md';
+	} else {
+		size = 'lg';
+	}
+	// console.log(`Size set: ${size}`);
+
+
+
+	// Define the dimensions and margin, width and height of the chart.
+	let margin = config.optional.margin[size];
+	let width = parseInt(graphic.style('width')) - margin.left - margin.right;
+	let height = 400 - margin.top - margin.bottom;
+	// console.log(`Margin, width, and height set: ${margin}, ${width}, ${height}`);
+
+	// Remove any existing chart elements
+	graphic.selectAll('*').remove();
+	//console.log(`Removed existing chart elements`);
+
+	// Get categories from the keys used in the stack generator
+	const categories = Object.keys(graphic_data[0]).filter((k) => k !== 'date');
+	// console.log(`Categories retrieved: ${categories}`);
+
+	// Define the x and y scales
+	const xAxis = d3
+		.scaleTime()
+		.domain(d3.extent(graphic_data, (d) => d.date))
+		.range([0, width]);
+	//console.log(`xAxis defined`);
+
+	const y = d3
+		.scaleLinear()
+		.domain([
+			0,
+			d3.max(graphic_data, (d) => Math.max(...categories.map((c) => d[c])))
+		])
+		.nice()
+		.range([height, 0]);
+	//console.log(`yAxis defined`);
+
+
+	// Create an SVG element
+	const svg = graphic
+		.append('svg')
+		.attr('width', width + margin.left + margin.right)
+		.attr('height', height + margin.top + margin.bottom)
+		.attr('class', 'chart')
+		.style('background-color', '#fff')
+		.append('g')
+		.attr('transform', `translate(${margin.left},${margin.top})`);
+	//console.log(`SVG element created`);
+
+
+
+	// create lines and circles for each category
+	categories.forEach(function (category) {
+		const lineGenerator = d3
+			.line()
+			.x((d) => xAxis(d.date))
+			.y((d) => y(d[category]))
+			.curve(d3[config.essential.lineCurveType]) // I used bracket notation here to access the curve type as it's a string
+			.context(null);
+		// console.log(`Line generator created for category: ${category}`);
+
+		svg
+			.append('path')
+			.datum(graphic_data)
+			.attr('fill', 'none')
+			.attr(
+				'stroke',
+				config.essential.colour_palette[
+				categories.indexOf(category) % config.essential.colour_palette.length
+				]
+			)
+			.attr('stroke-width', 3)
+			.attr('d', lineGenerator)
+			.style('stroke-linejoin', 'round')
+			.style('stroke-linecap', 'round');
+		//console.log(`Path appended for category: ${category}`);
+
+		const lastDatum = graphic_data[graphic_data.length - 1];
+
+
+		// console.log(`drawLegend: ${size}`);
+		// size === 'sm'
+		
+		if (config.essential.drawLegend || size === 'sm') {
+	
+
+				// Set up the legend
+			var legenditem = d3
+			.select('#legend')
+			.selectAll('div.legend--item')
+			.data(categories.map((c, i) => [c, config.essential.colour_palette[i % config.essential.colour_palette.length]]))
+			.enter()
+			.append('div')
+			.attr('class', 'legend--item');
+
+		legenditem
+			.append('div')
+			.attr('class', 'legend--icon--circle')
+			.style('background-color', function (d) {
+				return d[1];
+			});
+
+		legenditem
+			.append('div')
+			.append('p')
+			.attr('class', 'legend--text')
+			.html(function (d) {
+				return d[0];
+			});
+
+		} else {
+
+		// Add text labels to the right of the circles
+		svg
+		.append('text')
+		.attr(
+			'transform',
+			`translate(${xAxis(lastDatum.date)}, ${y(lastDatum[category])})`
+		)
+		.attr('x', 10)
+		.attr('dy', '.35em')
+		.attr('text-anchor', 'start')
+		.attr(
+			'fill',
+			config.essential.colour_palette[
+			categories.indexOf(category) % config.essential.colour_palette.length
+			]
+		)
+		.text(category)
+		.call(wrap, margin.right - 10); //wrap function for the direct labelling.
+			
+		};
+
+
+		svg
+			.append('circle')
+			.attr('cx', xAxis(lastDatum.date))
+			.attr('cy', y(lastDatum[category]))
+			.attr('r', 4)
+			.attr(
+				'fill',
+				config.essential.colour_palette[
+				categories.indexOf(category) % config.essential.colour_palette.length
+				]
+			);
+		// console.log(`Circle appended for category: ${category}`);
+
+
+
+	});
+
+	// add grid lines to y axis
+	svg
+		.append('g')
+		.attr('class', 'grid')
+		.call(
+			d3
+				.axisLeft(y)
+				.ticks(config.optional.yAxisTicks[size])
+				.tickSize(-width)
+				.tickFormat('')
+		);
+		
+
+// This function generates an array of approximately count + 1 uniformly-spaced, rounded values in the range of the given start and end dates (or numbers).
+let tickValues = xAxis.ticks(config.optional.xAxisTicks[size]);
+
+// Add the first and last dates to the ticks array, and use a Set to remove any duplicates
+tickValues = Array.from(new Set([graphic_data[0].date, ...tickValues, graphic_data[graphic_data.length - 1].date]));
+
+//console.log(`tickValues: ${tickValues}`);
+
+// Add the x-axis
+svg
+    .append('g')
+    .attr('class', 'x axis')
+    .attr('transform', `translate(0, ${height})`)
+    .call(
+        d3
+            .axisBottom(xAxis)
+            .tickValues(tickValues)
+            .tickFormat(d3.timeFormat(config.essential.xAxisTickFormat[size]))
+    );
+
+
+	// Add the y-axis
+	svg
+		.append('g')
+		.attr('class', 'y axis')
+		.call(d3.axisLeft(y).ticks(config.optional.yAxisTicks[size]));
+
+	
+
+	// This does the x-axis label
+	svg
+		.append('g')
+		.attr('transform', `translate(0, ${height})`)
+		.append('text')
+		.attr('x', width)
+		.attr('y', 35)
+		.attr('class', 'axis--label')
+		.text(config.essential.xAxisLabel)
+		.attr('text-anchor', 'end');
+
+	//create link to source
+	d3.select('#source').text('Source: ' + config.essential.sourceText);
+	// console.log(`Link to source created`);
+
+	//use pym to calculate chart dimensions
+	if (pymChild) {
+		pymChild.sendHeight();
+	}
+	// console.log(`PymChild height sent`);
+}
+
+//text wrap function for the direct labelling
+
+function wrap(text, width) {
+	text.each(function () {
+		var text = d3.select(this),
+			words = text.text().split(/\s+/).reverse(),
+			word,
+			line = [],
+			lineNumber = 0,
+			lineHeight = 1.1, // ems
+			// y = text.attr("y"),
+			x = text.attr('x'),
+			dy = parseFloat(text.attr('dy')),
+			tspan = text.text(null).append('tspan').attr('x', x);
+		while ((word = words.pop())) {
+			line.push(word);
+			tspan.text(line.join(' '));
+			if (tspan.node().getComputedTextLength() > width) {
+				line.pop();
+				tspan.text(line.join(' '));
+				line = [word];
+				tspan = text
+					.append('tspan')
+					.attr('x', x)
+					.attr('dy', lineHeight + 'em')
+					.text(word);
+			}
+		}
+		var breaks = text.selectAll('tspan').size();
+		text.attr('y', function () {
+			return -6 * (breaks - 1);
+		});
+	});
+}
+
+// Load the data
+d3.csv(config.essential.graphic_data_url).then((rawData) => {
+	graphic_data = rawData.map((d) => {
+		return {
+			date: d3.timeParse(config.essential.dateFormat)(d.date),
+			...Object.entries(d)
+				.filter(([key]) => key !== 'date')
+				.map(([key, value]) => [key, +value])
+				.reduce((acc, [key, value]) => ({ ...acc, [key]: value }), {})
+		};
+	});
+
+	// console.log(`Data from CSV processed`);
+
+	// console.log('Final data structure:');
+	// console.log(graphic_data);
+
+	// Use pym to create an iframed chart dependent on specified variables
+	pymChild = new pym.Child({
+		renderCallback: drawGraphic
+	});
+	// console.log(`PymChild created with renderCallback to drawGraphic`);
+});
