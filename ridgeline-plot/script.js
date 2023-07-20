72dd0753c1c818f123e12c45faefff1580ddc0ba
Micheal Slade <50627681+Micheal-Slade@users.noreply.github.com>
2023-07-20 15:57:58 +0100

    turning off z scale

--- ridgeline-plot/script.js
+++ ridgeline-plot/script.js
@@ -60,9 +60,9 @@ function drawGraphic() {
 		.range([height, 0])
 		.padding(1);
 
-		const z = d3.scaleOrdinal()
-		.range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"])
-		.domain(keys);
+		// const z = d3.scaleOrdinal()
+		// .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"])
+		// .domain(keys);
 
 		let z = d3.scaleSequential(d3.interpolateCool).domain([0, keys.length]);
 
