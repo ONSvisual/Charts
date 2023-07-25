c20b91baf8cb76081ae9474a405e57a21b923a0a
MikeLister <131891138+MikeLister@users.noreply.github.com>
2023-07-25 15:26:23 +0100

    New template - small multiple stacked column

---
+++ bar-chart-vertical-stacked-sm/config.js
@@ -0,0 +1,77 @@
+config = {
+	"essential": {
+		"graphic_data_url": "data.csv",
+		"colour_palette": ["#206095", "#27A0CC", "#871A5B", "#A8BD3A", "#F66068"],
+		"sourceText": "Office for National Statistics",
+		"accessibleSummary": "Here is the screenreader text describing the chart.",
+		"xAxisTickFormat": {
+			"sm": "%Y",
+			"md": "%Y",
+			"lg": "%b-%y"
+		},
+		"dateFormat": "%b-%y",
+		//the format your date data has in data.csv
+		"yDomain": "auto",
+		// either "auto" or an array for the x domain e.g. [0,100]
+		"yAxisLabel": "y axis label",
+		"stackOffset": "stackOffsetNone",
+		// options include
+		// stackOffsetNone means the baseline is set at zero
+		// stackOffsetExpand to do 100% charts
+		// stackOffsetDiverging for data with positive and negative values
+		"stackOrder": "stackOrderNone"
+		// other options include
+		// stackOrderNone means the order is taken from the datafile
+		// stackOrderAppearance the earliest series (according to the maximum value) is at the bottom
+		// stackOrderAscending the smallest series (according to the sum of values) is at the bottom
+		// stackOrderDescending the largest series (according to the sum of values) is at the bottom
+		// stackOrderReverse reverse the order as set from the data file
+	},
+	"optional": {
+		"showStartEndDate": false,
+		"margin": {
+			"sm": {
+				"top": 40,
+				"right": 20,
+				"bottom": 50,
+				"left": 70
+			},
+			"md": {
+				"top": 40,
+				"right": 20,
+				"bottom": 50,
+				"left": 70
+			},
+			"lg": {
+				"top": 40,
+				"right": 20,
+				"bottom": 50,
+				"left": 70
+			}
+		},
+		"chart_every": {
+			"sm": 1,
+			"md": 2,
+			"lg": 2
+		},
+		"aspectRatio": {
+			"sm": [1, 1],
+			"md": [1, 1],
+			"lg": [1, 1]
+		},
+		"xAxisTicksEvery": {
+			"sm": 4,
+			"md": 4,
+			"lg": 3
+		},
+		"yAxisTicks": {
+			"sm": 4,
+			"md": 8,
+			"lg": 10
+		},
+		"mobileBreakpoint": 510,
+		"mediumBreakpoint": 600
+	},
+	"elements": { "select": 0, "nav": 0, "legend": 0, "titles": 0 },
+	"chart_build": {}
+};
