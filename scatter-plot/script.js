3be2b72df768afa64bc1b19c0c6718f6df14fc1a
Mike Lister <mike.lister@ons.gov.uk>
2023-09-20 14:20:04 +0100

    Scatterplot y-domain fix

--- scatter-plot/script.js
+++ scatter-plot/script.js
@@ -87,10 +87,10 @@ legenditem
   }
 
 
-  if(config.essential.xDomain=="auto"){
+  if(config.essential.yDomain=="auto"){
     y.domain([0, d3.max(graphic_data,function(d){return d.yvalue})]);
   }else{
-    y.domain(config.essential.xDomain)
+    y.domain(config.essential.yDomain)
   }
 
   svg
