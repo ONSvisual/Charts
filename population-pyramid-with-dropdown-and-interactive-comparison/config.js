c9bdcc14aba00073cb4fb216c5e797b862f5b701
Mike Lister <mike.lister@ons.gov.uk>
2023-12-19 09:44:56 +0000

    Moving y axis label to config

--- population-pyramid-with-dropdown-and-interactive-comparison/config.js
+++ population-pyramid-with-dropdown-and-interactive-comparison/config.js
@@ -12,7 +12,8 @@ config = {
 		// this is the lighter palette for reference lines ["#9A86E9", "#3fb0b3"]
 		"comparison_colour_palette": ["#5c5185", "#306970"],
 		"legend": ["Selected area", "2011"],
-		"xAxislabel": ["Percentage"]
+		"xAxislabel": ["Percentage"],
+		"yAxislabel": ["Age"]
 	},
 	"optional": {
 		"margin": {
