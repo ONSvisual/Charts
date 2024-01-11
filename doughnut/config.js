ee30fafb7aea60e186a5607b83b39d9cbc90a6bb
MikeLister <131891138+MikeLister@users.noreply.github.com>
2024-01-11 11:11:58 +0000

    Add files via upload
    Adding doughnut chart template

---
+++ doughnut/config.js
@@ -0,0 +1,39 @@
+config = {
+	"essential": {
+		"graphic_data_url": "data.csv",
+		"colour_palette": ["#206095", "#27A0CC", "#871A5B", "#A8BD3A", "#F66068", "#003C57"],
+		"sourceText": "Office for National Statistics",
+		"accessibleSummary": "",
+		"dataLabels": {
+			"show": true,
+			"numberFormat": ".1%"
+		},
+		"centreLabel": "Sikh identity"
+	},
+	"optional": {
+		"margin": {
+			"sm": {
+				"top": 15,
+				"right": 20,
+				"bottom": 50,
+				"left": 20
+			},
+			"md": {
+				"top": 20,
+				"right": 80,
+				"bottom": 90,
+				"left": 80
+			},
+			"lg": {
+				"top": 20,
+				"right": 60,
+				"bottom": 70,
+				"left": 60
+			}
+		},
+		"mobileBreakpoint": 510,
+		"mediumBreakpoint": 600
+	},
+	"elements": { "select": 0, "nav": 0, "legend": 0, "titles": 0 },
+	"chart_build": {}
+};
