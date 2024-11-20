718eaaa8d54174aec16316b859d4676df085f383
henryjameslau <2945099+henryjameslau@users.noreply.github.com>
2024-11-20 11:36:10 +0000

    Add folder as safe directory

--- refresh.bat
+++ refresh.bat
@@ -1,3 +1,4 @@
 echo Refreshing library to latest version
+git config --global --add safe.directory '%(prefix)///fa1rvwwsxx343/datavisweb/repos/Charts'
 git fetch --all
 git reset --hard origin/main
