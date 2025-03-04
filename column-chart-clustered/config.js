0825024aec5bd5bb0ee98154e851ae2cc7cbfef0
Sam Cottrell <sam.ctrl.ons@gmail.com>
2025-03-04 13:23:05 +0000

    Added a clustered column chart template

---
+++ column-chart-clustered/config.js
@@ -0,0 +1,63 @@
+config = {
+	"essential": {
+		"graphic_data_url": "data.csv",
+		"colour_palette": ["#206095", "#27A0CC", "#871A5B", "#A8BD3A", "#F66068", "#003C57"],
+		"sourceText": "Office for National Statistics",
+		"accessibleSummary": "The chart canvas is hidden from screen readers. The main message is summarised by the chart title and the data behind the chart is available to download below.",
+		"dataLabels": {
+			"show": true,
+			"numberFormat": ".0%"
+		},
+		"xDomain": "auto",
+		// either "auto" or an array for the x domain e.g. [0,100]
+		"xAxisLabel": "",
+		"chartHeight": {
+			"sm": 300,
+			"md": 300,
+			"lg": 300
+		},
+	},
+	"optional": {
+		"chart_every": {
+			"sm": 1,
+			"md": 1,
+			"lg": 1
+		},
+		// "aspectRatio": {
+		// 	"sm": [1, 2],
+		// 	"md": [1, 2],
+		// 	"lg": [1, 2]
+		// },
+		"margin": {
+			"sm": {
+				"top": 30,
+				"right": 20,
+				"bottom": 50,
+				"left": 50
+			},
+			"md": {
+				"top": 30,
+				"right": 20,
+				"bottom": 50,
+				"left": 50
+			},
+			"lg": {
+				"top": 30,
+				"right": 20,
+				"bottom": 50,
+				"left": 50
+			}
+		},
+		"chartGap": 10,
+		"xAxisTicks": {
+			"sm": 2,
+			"md": 2,
+			"lg": 4
+		},
+		"mobileBreakpoint": 510,
+		"mediumBreakpoint": 600,
+		"dropYAxis": true
+	},
+	"elements": { "select": 0, "nav": 0, "legend": 0, "titles": 0 },
+	"chart_build": {}
+};
