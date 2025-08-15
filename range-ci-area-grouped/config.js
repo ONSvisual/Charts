9e09a5ac5f5c9bfc3006bd3af973b74de40be3bb
watsos <sam.watson@ons.gov.uk>
2025-08-15 12:44:22 +0100

    Adding option to config to make the chart be clustered

--- range-ci-area-grouped/config.js
+++ range-ci-area-grouped/config.js
@@ -4,6 +4,7 @@ config = {
 		"colour_palette": [
 			"#206095",
 			"#871A5B",
+			"#8D8C8E",
 			"#27A0CC",
 			"#A8BD3A",
 			"#F66068",
@@ -14,11 +15,14 @@ config = {
 			"A range plot with confidence intervals. This chart is hidden from screen readers. The main message is summarised by the chart title and the data behind the chart is available to download below.",
 		"xAxisTickFormat": ".0f",
 		"xAxisLabel": "x axis label",
-		"xDomain": [0, 160],
+		"xDomain": [0,160],
 		// either auto or a custom domain as an array e.g [0,100]
 		"CI_legend": true,
 		"CI_legend_interval_text": "Likely range (95% confidence interval)",
-		"CI_legend_text": "Estimated value"
+		"CI_legend_text": "Estimated value",
+		// If you have a third series, make sure to add it at the end of the script for the time being. This needs to be added as a reference option in config.
+		// If you want a clustered chart, true
+		"clustered": true, 
 	},
 	"optional": {
 		"margin": {
