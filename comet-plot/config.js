5e5ca8e2ccddd491814f7b274044f25fef48a571
henryjameslau <henry.lau@ons.gov.uk>
2025-08-29 11:15:52 +0100

    Edit the configs (so many)

--- comet-plot/config.js
+++ comet-plot/config.js
@@ -1,62 +1,59 @@
 config = {
-	"essential": {
-		"graphic_data_url": "data.csv",
-		"legendLabels": { "min": "2011", "max": "2021" },
-		//the keys match the column names
-		"colour_palette": ["#206095", "#F66068", "#A09FA0"],
-		"sourceText": "Office for National Statistics",
-		"accessibleSummary":
-			"Here is the screenreader text describing the chart.",
-		"numberFormat": ".0f",
-		"xAxisNumberFormat": ".0f",
-		"xAxisLabel": "x axis label",
-		"xDomain": [0, 100],
-		// either auto or a custom domain as an array e.g [0,100]
-		"dotsize": 6,
-		"legendItems": ["Inc","Dec","No"],
-		//Choose which items to include in the legend, and the order that they appear
-		"legendLineLength": 60,
-		"legendItemWidth": 150,
-		"showDataLabels": true
+	"graphic_data_url": "data.csv",
+	"legendLabels": { "min": "2011", "max": "2021" },
+	//the keys match the column names
+	"colour_palette": ["#206095", "#F66068", "#A09FA0"],
+	"sourceText": "Office for National Statistics",
+	"accessibleSummary":
+		"Here is the screenreader text describing the chart.",
+	"numberFormat": ".0f",
+	"xAxisNumberFormat": ".0f",
+	"xAxisLabel": "x axis label",
+	"xDomain": [0, 100],
+	// either auto or a custom domain as an array e.g [0,100]
+	"dotsize": 6,
+	"legendItems": ["Inc", "Dec", "No"],
+	//Choose which items to include in the legend, and the order that they appear
+	"legendLineLength": 60,
+	"legendItemWidth": 150,
+	"showDataLabels": true,
+
+	"margin": {
+		"sm": {
+			"top": 5,
+			"right": 20,
+			"bottom": 20,
+			"left": 100
+		},
+		"md": {
+			"top": 5,
+			"right": 20,
+			"bottom": 20,
+			"left": 100
+		},
+		"lg": {
+			"top": 5,
+			"right": 20,
+			"bottom": 40,
+			"left": 100
+		}
 	},
-	"optional": {
-		"margin": {
-			"sm": {
-				"top": 5,
-				"right": 20,
-				"bottom": 20,
-				"left": 100
-			},
-			"md": {
-				"top": 5,
-				"right": 20,
-				"bottom": 20,
-				"left": 100
-			},
-			"lg": {
-				"top": 5,
-				"right": 20,
-				"bottom": 40,
-				"left": 100
-			}
-		},
-		"seriesHeight": {
-			"sm": 40,
-			"md": 40,
-			"lg": 40
-		},
-		"xAxisTicks": {
-			"sm": 4,
-			"md": 8,
-			"lg": 10
-		},
-		"legendHeight": {
-			"sm": 40,
-			"md": 40,
-			"lg": 40
-		},
-		"mobileBreakpoint": 510,
-		"mediumBreakpoint": 600
+	"seriesHeight": {
+		"sm": 40,
+		"md": 40,
+		"lg": 40
 	},
+	"xAxisTicks": {
+		"sm": 4,
+		"md": 8,
+		"lg": 10
+	},
+	"legendHeight": {
+		"sm": 40,
+		"md": 40,
+		"lg": 40
+	},
+	"mobileBreakpoint": 510,
+	"mediumBreakpoint": 600,
 	"elements": { "select": 0, "nav": 0, "legend": 1, "titles": 0 }
 };
--- line-chart-with-ci-area-sm/config.js
+++ line-chart-with-ci-area-sm/config.js
@@ -1,87 +1,84 @@
 config = {
-	"essential": {
-		"graphic_data_url": "data.csv",
-		"colour_palette": [
-			"#206095",
-			"#871A5B",
-			"#27A0CC",
-			"#A8BD3A",
-			"#F66068",
-			"#118C7B"
-		],
-		"sourceText": "Annual Population Survey from the Office for National Statistics ",
-		"accessibleSummary": "Here is the screen reader text describing the chart.",
-		"lineCurveType": "curveLinear", // Set the default line curve type
-		// Examples of line curve types
-		// "lineCurveType": "curveLinear", // Straight line segments
-		// "lineCurveType": "curveStep", // Step-wise line
-		// "lineCurveType": "curveStepBefore", // Step-before line
-		// "lineCurveType": "curveStepAfter", // Step-after line
-		// "lineCurveType": "curveBasis", // B-spline curve
-		// "lineCurveType": "curveCardinal", // Cardinal spline curve
-		// "lineCurveType": "curveCatmullRom" // Catmull-Rom spline curve
-		// "lineCurveType": "curveMonotoneX" // Monotone spline curve
+	"graphic_data_url": "data.csv",
+	"colour_palette": [
+		"#206095",
+		"#871A5B",
+		"#27A0CC",
+		"#A8BD3A",
+		"#F66068",
+		"#118C7B"
+	],
+	"sourceText": "Annual Population Survey from the Office for National Statistics ",
+	"accessibleSummary": "Here is the screen reader text describing the chart.",
+	"lineCurveType": "curveLinear", // Set the default line curve type
+	// Examples of line curve types
+	// "lineCurveType": "curveLinear", // Straight line segments
+	// "lineCurveType": "curveStep", // Step-wise line
+	// "lineCurveType": "curveStepBefore", // Step-before line
+	// "lineCurveType": "curveStepAfter", // Step-after line
+	// "lineCurveType": "curveBasis", // B-spline curve
+	// "lineCurveType": "curveCardinal", // Cardinal spline curve
+	// "lineCurveType": "curveCatmullRom" // Catmull-Rom spline curve
+	// "lineCurveType": "curveMonotoneX" // Monotone spline curve
 
-		"xDomain": [-13,25],
-		// either "auto" or an array for the x domain e.g. [0,2000] - DOES NOT WORK
-		"xAxisTickFormat": {
-			"sm": "%Y",
-			"md": "%Y",
-			"lg": "%Y"
-		},
-		"yAxisFormat": ",.0%",
-		"dateFormat": "%Y",
-		"yAxisLabel": "Pay Gap",
-		"CI_legend": true,
-		"CI_legend_interval_text": "Likely range (95% confidence interval)",
-		"CI_legend_text": "Estimated value",
-		"zeroLine": "0"
+	"xDomain": [-13, 25],
+	// either "auto" or an array for the x domain e.g. [0,2000] - DOES NOT WORK
+	"xAxisTickFormat": {
+		"sm": "%Y",
+		"md": "%Y",
+		"lg": "%Y"
 	},
-	"optional": {
-		"chart_every": {
-			"sm": 1,
-			"md": 2,
-			"lg": 2
-		},
-		"aspectRatio": {
-			"sm": [1.2, 1],
-			"md": [1.2, 1],
-			"lg": [1.2, 1]
-		},
-		"margin": {
-			"sm": {
-				"top": 70,
-				"right": 15,
-				"bottom": 50,
-				"left": 45
-			},
-			"md": {
-				"top": 70,
-				"right": 25,
-				"bottom": 50,
-				"left": 45
-			},
-			"lg": {
-				"top": 70,
-				"right": 25,
-				"bottom": 50,
-				"left": 45
-			}
-		},
-		"chartGap": 20,
-		"xAxisTicksEvery": { // this is the interval of ticks on the x axis but it will always show the first and last date.
-			"sm": 2,
-			"md": 2,
-			"lg": 2
-		},
-		"yAxisTicks": {
-			"sm": 7,
-			"md": 5,
-			"lg":8
-		},
-		"mobileBreakpoint": 510,
-		"mediumBreakpoint": 600,
-		"dropYAxis": true
+	"yAxisFormat": ",.0%",
+	"dateFormat": "%Y",
+	"yAxisLabel": "Pay Gap",
+	"CI_legend": true,
+	"CI_legend_interval_text": "Likely range (95% confidence interval)",
+	"CI_legend_text": "Estimated value",
+	"zeroLine": "0",
+
+	"chart_every": {
+		"sm": 1,
+		"md": 2,
+		"lg": 2
 	},
+	"aspectRatio": {
+		"sm": [1.2, 1],
+		"md": [1.2, 1],
+		"lg": [1.2, 1]
+	},
+	"margin": {
+		"sm": {
+			"top": 70,
+			"right": 15,
+			"bottom": 50,
+			"left": 45
+		},
+		"md": {
+			"top": 70,
+			"right": 25,
+			"bottom": 50,
+			"left": 45
+		},
+		"lg": {
+			"top": 70,
+			"right": 25,
+			"bottom": 50,
+			"left": 45
+		}
+	},
+	"chartGap": 20,
+	"xAxisTicksEvery": { // this is the interval of ticks on the x axis but it will always show the first and last date.
+		"sm": 2,
+		"md": 2,
+		"lg": 2
+	},
+	"yAxisTicks": {
+		"sm": 7,
+		"md": 5,
+		"lg": 8
+	},
+	"mobileBreakpoint": 510,
+	"mediumBreakpoint": 600,
+	"dropYAxis": true,
 	"elements": { "select": 0, "nav": 0, "legend": 1, "titles": 0 }
 };
