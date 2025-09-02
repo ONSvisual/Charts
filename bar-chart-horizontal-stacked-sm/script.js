d581107d6cff3a7dbfb174ef36670532ceb59210
sam-ctrl <sam.cottrell@ons.gov.uk>
2025-09-02 14:55:07 +0100

    Overhauled stacked horizontal bar chart small multiple template

--- bar-chart-horizontal-stacked-sm/script.js
+++ bar-chart-horizontal-stacked-sm/script.js
@@ -1,263 +1,240 @@
-import { initialise, wrap, addSvg, calculateChartWidth, addAxisLabel, addSource } from "../lib/helpers.js";
+import { initialise, wrap, addSvg, calculateChartWidth, addChartTitleLabel, addAxisLabel, addSource, addAnnotationRangeHorizontal } from "../lib/helpers.js";
 
-let pymChild = null;
 let graphic = d3.select('#graphic');
 let legend = d3.select('#legend');
-let size, graphic_data;
-
-
+let pymChild = null;
+let graphic_data, size;
 
 function drawGraphic() {
+    //Set up some of the basics and return the size value ('sm', 'md' or 'lg')
+    size = initialise(size);
 
-	//Set up some of the basics and return the size value ('sm', 'md' or 'lg')
-	size = initialise(size);
+    const aspectRatio = config.optional.aspectRatio[size];
+    const chartsPerRow = config.optional.chart_every[size];
 
-	// Group the data by the 'series' column
-	const nested_data = d3.groups(graphic_data, (d) => d.series);
+    // Get categories from the keys used in the stack generator
+    const categories = graphic_data.columns.slice(2);
 
-	//Generate a list of categories based on the order in the first chart that we can use to order the subsequent charts
-	let namesArray = [...nested_data][0][1].map(d => d.name);
+    // Nest the graphic_data by the 'series' column
+    let nested_data = d3.group(graphic_data, (d) => d.series);
 
-	// Create a container div for each small multiple
-	let chartContainers = graphic
-		.selectAll('.chart-container')
-		.data(Array.from(nested_data))
-		.join('div')
-		.attr('class', 'chart-container');
+    // Create a container div for each small multiple
+    let chartContainers = graphic
+        .selectAll('.chart-container')
+        .data(Array.from(nested_data))
+        .join('div')
+        .attr('class', 'chart-container');
 
-	function drawChart(container, seriesName, data, chartIndex) {
-		//Sort the data so that the bars in each chart are in the same order
-		data.sort((a, b) => namesArray.indexOf(a.name) - namesArray.indexOf(b.name))
+    function drawChart(container, data, seriesName, chartIndex) {
+        let chartPosition = chartIndex % chartsPerRow;
+        let margin = { ...config.optional.margin[size] };
+        let chartGap = config.optional?.chartGap || 10;
+        let chart_width = calculateChartWidth({
+            screenWidth: parseInt(graphic.style('width')),
+            chartEvery: chartsPerRow,
+            chartMargin: margin,
+            chartGap: chartGap
+        });
 
-		const chartsPerRow = config.optional.chart_every[size];
-		const chartPosition = chartIndex % chartsPerRow;
+        if (config.optional.dropYAxis) {
+            if (chartPosition !== 0) {
+                margin.left = chartGap;
+            }
+        }
 
-		// Set dimensions
-		let margin = { ...config.optional.margin[size] };
-		let chartGap = config.optional?.chartGap || 10;
+        let height = aspectRatio[1] / aspectRatio[0] * chart_width;
 
-		let chart_width = calculateChartWidth({
-			screenWidth: parseInt(graphic.style('width')),
-			chartEvery: chartsPerRow,
-			chartMargin: margin,
-			chartGap: chartGap
-		})
+        // Flip axes: x is value, y is category
+        const x = d3.scaleLinear().range([0, chart_width]);
+        const y = d3.scaleBand().paddingOuter(0.0).paddingInner(0.1).range([0, height]).round(true);
 
-		// If the chart is not in the first position in the row, reduce the left margin
-		if (config.optional.dropYAxis) {
-			if (chartPosition !== 0) {
-				margin.left = chartGap;
-			}
-		}
+        y.domain([...new Set(graphic_data.map((d) => d.date))]);
 
+        const stack = d3.stack()
+            .keys(categories)
+            .offset(d3[config.essential.stackOffset])
+            .order(d3[config.essential.stackOrder]);
 
-		//console.log(`The value of chart_width is ${chart_width}.`);
+        const series = stack(data);
+        const seriesAll = stack(graphic_data);
 
-		//height is set by unique options in column name * a fixed height + some magic because scale band is all about proportion
+        if (config.essential.yDomain == 'auto') {
+            x.domain(d3.extent(seriesAll.flat(2)));
+        } else {
+            x.domain(config.essential.yDomain);
+        }
 
-		let height =
-			config.optional.seriesHeight[size] * data.length +
-			10 * (data.length - 1) +
-			12;
+        // Create an SVG element
+        const svg = addSvg({
+            svgParent: container,
+            chart_width: chart_width,
+            height: height + margin.top + margin.bottom,
+            margin: margin
+        });
 
-		// Define scales
-		const x = d3.scaleLinear().range([0, chart_width]);
+        // Draw bars
+        svg.append('g')
+            .selectAll('g')
+            .data(series)
+            .join('g')
+            .attr('fill', (d, i) => config.essential.colour_palette[i])
+            .selectAll('rect')
+            .data((d) => d)
+            .join('rect')
+            .attr('x', (d) => x(d[0]))
+            .attr('y', (d) => y(d.data.date))
+            .attr('width', (d) => Math.abs(x(d[1]) - x(d[0])))
+            .attr('height', y.bandwidth());
 
-		// This is a different version
-		// const y = d3.scaleBand().rangeRound([0, height]).padding(0.1);
+        // Add grid lines to x axis
+        const gridG = svg.append('g')
+            .attr('class', 'grid')
+            .call(
+                d3.axisBottom(x)
+                .ticks(config.optional.yAxisTicks[size])
+                .tickSize(-height)
+                .tickFormat('')
+            )
+            .attr('transform', `translate(0,${height})`)
+            .lower();
 
-		const y = d3
-			.scaleBand()
-			.paddingOuter(0.2)
-			.paddingInner(((data.length - 1) * 10) / (data.length * 30))
-			.range([0, height])
-			.round(true);
+        // Ensure grid tick lines have zero-line class for correct styling
+        gridG.selectAll('.tick line')
+            .attr('class', function(d) {
+                return d === 0 ? 'zero-line' : null;
+            });
 
-		// Define axes
-		let xAxis = d3
-			.axisBottom(x)
-			.tickSize(-height)
-			.tickFormat(d3.format(config.essential.xAxisTickFormat))
-			.ticks(config.optional.xAxisTicks[size]);
+        // Add the x-axis
+        svg.append('g')
+            .attr('class', 'x axis')
+            .attr('transform', `translate(0,${height})`)
+            .call(d3.axisBottom(x)
+                .ticks(config.optional.yAxisTicks[size])
+                .tickFormat(d3.format('.0f'))
+            );
 
-		let yAxis = d3.axisLeft(y)
-			.tickSize(0)
-			.tickPadding(10)
-			.tickFormat((d) => config.optional.dropYAxis !== true ? (d) :
-				chartPosition == 0 ? (d) : "");
+        // Ensure tick lines and zero line use correct CSS classes for x axis
+        svg.selectAll('g.x.axis .tick line')
+            .attr('class', function(d) {
+                return d === 0 ? 'zero-line' : null;
+            });
 
-		// Define stack layout
-		let stack = d3
-			.stack()
-			.offset(d3[config.essential.stackOffset])
-			.order(d3[config.essential.stackOrder])
-			.keys(graphic_data.columns.slice(1, -1));
+        // Only draw the y axis tick labels on the first chart in each row
+        if (chartIndex % chartsPerRow === 0) {
+            svg.append('g')
+                .attr('class', 'y axis category')
+                .call(d3.axisLeft(y).tickValues(y.domain()))
+                .selectAll('.tick text')
+                .call(wrap, margin.left - 10);
+        } else {
+            svg.append('g').attr('class', 'y axis category').call(d3.axisLeft(y).tickValues([]));
+        }
 
-		// Process data ! This needs review
+        // Ensure tick lines and zero line use correct CSS classes for y axis
+        svg.selectAll('g.y.axis.category .tick line')
+            .attr('class', function(d) {
+                return d === 0 ? 'zero-line' : null;
+            });
 
-		const series = stack(data);
+        // This does the chart title label
+        addChartTitleLabel({
+            svgContainer: svg,
+            yPosition: -margin.top / 2,
+            text: seriesName,
+            wrapWidth: (chart_width + margin.right)
+        });
 
-		// console.table(series);
+        // This does the y-axis label
+        if (chartIndex % chartsPerRow === 0) {
+            addAxisLabel({
+                svgContainer: svg,
+                xPosition: 5 - margin.left,
+                yPosition: 0,
+                text: config.essential.yAxisLabel,
+                textAnchor: "start",
+                wrapWidth: chart_width
+            });
+        }
 
-		if (config.essential.xDomain === 'auto') {
-			x.domain([Math.min(0, d3.min(stack(graphic_data), (d) => d3.min(d, (d) => d[1]))), d3.max(stack(graphic_data), (d) => d3.max(d, (d) => d[1]))]); //removed.nice()
-		} else {
-			x.domain(config.essential.xDomain);
-		}
+        // This does the x-axis label
+        if (chartIndex % chartsPerRow === chartsPerRow - 1 || chartIndex === [...chartContainers].length - 1) {
+            addAxisLabel({
+                svgContainer: svg,
+                xPosition: chart_width,
+                yPosition: height + 35,
+                text: config.essential.xAxisLabel,
+                textAnchor: "end",
+                wrapWidth: chart_width
+            });
+        }
 
-		y.domain(data.map((d) => d.name));
+        // Add annotation range if needed
+        if (chartIndex !== 0) {
+            addAnnotationRangeHorizontal(
+                svg,
+                chart_width,
+                y("two years") + (4 - chartIndex) * y.step(),
+                y("five years") + y.bandwidth(),
+                chartIndex == 1 ? 'N/A' : "",
+                'above',
+                'inside',
+                x(5),
+                50
+            );
+        }
+    }
 
-		// Set up the legend
-		let legenditem = legend
-			.selectAll('div.legend--item')
-			.data(
-				d3.zip(graphic_data.columns.slice(1), config.essential.colour_palette)
-			)
-			.enter()
-			.append('div')
-			.attr('class', 'legend--item');
+    // Draw the charts for each small multiple
+    chartContainers.each(function ([key, value], i) {
+        drawChart(d3.select(this), value, key, i);
+    });
 
-		legenditem
-			.append('div')
-			.attr('class', 'legend--icon--circle')
-			.style('background-color', function (d) {
-				return d[1];
-			});
+    // Set up the legend
+    let legenditem = legend
+        .selectAll('div.legend--item')
+        .data(categories.map((cat, i) => [cat, config.essential.colour_palette[i]]))
+        .enter()
+        .append('div')
+        .attr('class', 'legend--item');
 
-		legenditem
-			.append('div')
-			.append('p')
-			.attr('class', 'legend--text')
-			.html(function (d) {
-				return d[0];
-			});
+    legenditem
+        .append('div')
+        .attr('class', 'legend--icon--circle')
+        .style('background-color', function (d) {
+            return d[1];
+        });
 
-		//End of legend code
+    legenditem
+        .append('div')
+        .append('p')
+        .attr('class', 'legend--text')
+        .html(function (d) {
+            return d[0];
+        });
 
-		// console.log('Chart width:', chart_width);
-		// console.log('Height:', height);
-		// console.log('X domain:', x.domain());
-		// console.log('Y domain:', y.domain());
+    // Set legend columns if config.optional.legendColumns is defined
+    if (config.optional.legendColumns) {
+        legend.style('display', 'grid')
+              .style('grid-template-columns', `repeat(${config.optional.legendColumns}, 1fr)`);
+    }
 
-		// Create SVG
-		let svg = addSvg({
-			svgParent: graphic,
-			chart_width: chart_width,
-			height: height + margin.top + margin.bottom,
-			margin: margin
-		})
+	svg.selectAll('.grid.domain').attr('display', 'none');
 
+    //create link to source
+    addSource('source', config.essential.sourceText);
 
-		// Add axes
-		svg
-			.append('g')
-			.attr('transform', 'translate(0,' + height + ')')
-			.attr('class', 'x axis')
-			.call(xAxis)
-			.selectAll('line')
-			.each(function (d) {
-				if (d == 0) {
-					d3.select(this).attr('class', 'zero-line');
-				}
-			});
-
-		// console.log(`The value of margin.left - (your value) is ${margin.left - 30}.`);
-
-		// if (chartIndex % chartsPerRow === 0) {
-		svg
-			.append('g')
-			.attr('class', 'y axis')
-			.call(yAxis)
-			.selectAll('.tick text')
-			.call(wrap, margin.left - 10);
-		// } else {
-		// 	svg.append('g').attr('class', 'y axis').call(yAxis.tickValues([]));
-		// }
-
-		// Add a bold text label to the top left corner of the chart SVG
-		svg
-			.append('text')
-			.attr('class', 'title')
-			.attr('x', 0)
-			.attr('y', -margin.top / 2)
-			.text(seriesName);
-
-		// Draw chart
-		svg
-			.append('g')
-			.selectAll('g')
-			.data(series)
-			.enter()
-			.append('g')
-			.style('fill', (_, i) => config.essential.colour_palette[i])
-			.selectAll('rect')
-			.data((d) => d)
-			.join('rect')
-			.attr('x', (d) => x(d[0]))
-			.attr('y', (d, i) => y(data[i].name))
-			.attr('width', (d) => x(d[1]) - x(d[0]))
-			.attr('height', y.bandwidth());
-
-		// This does the x-axis label
-		if (chartIndex % chartsPerRow === chartsPerRow - 1 || chartIndex === nested_data.length - 1) {
-			addAxisLabel({
-				svgContainer: svg,
-				xPosition: chart_width,
-				yPosition: height + 35,
-				text: config.essential.xAxisLabel,
-				textAnchor: "end",
-				wrapWidth: chart_width
-			});
-		}
-	}
-	// Draw the charts for each small multiple
-	chartContainers.each(function ([key, value], i) {
-		drawChart(d3.select(this), key, value, i);
-	});
-
-	//create link to source
-	addSource('source', config.essential.sourceText);
-
-	//use pym to calculate chart dimensions
-	if (pymChild) {
-		pymChild.sendHeight();
-	}
+    //use pym to calculate chart dimensions
+    if (pymChild) {
+        pymChild.sendHeight();
+    }
 }
 
-
 // Load the data
-d3.csv(config.essential.graphic_data_url)
-	.then((data) => {
-		// console.log('Original data:', data);
 
-		// // clear out existing graphics
-		// graphic.selectAll('*').remove();
-		// legend.selectAll('*').remove();
+d3.csv(config.essential.graphic_data_url).then((data) => {
+    graphic_data = data;
 
-		graphic_data = data;
-		// // Group the data by the 'series' column
-		// const groupedData = d3.groups(fullData, (d) => d.series);
-		// // console.log('Grouped data:', groupedData[0][1]);
-
-		// //Generate a list of categories based on the order in the first chart that we can use to order the subsequent charts
-		// let namesArray = [...groupedData][0][1].map(d => d.name);
-
-
-		// groupedData.forEach((group, i) => {
-		// 	const seriesName = group[0];
-		// 	const graphic_data = group[1];
-
-		// 	//Sort the data so that the bars in each chart are in the same order
-		// 	graphic_data.sort((a, b) => namesArray.indexOf(a.name) - namesArray.indexOf(b.name))
-
-		// 	graphic_data.columns = data.columns;
-
-		// 	pymChild = new pym.Child({ renderCallback: drawGraphic(seriesName, graphic_data, i, groupedData.length, fullData) });
-		// });
-
-		//use pym to create iframed chart dependent on specified variables
-		pymChild = new pym.Child({
-			renderCallback: drawGraphic
-		});
-	})
-	.catch((error) => console.error(error));
+    pymChild = new pym.Child({
+        renderCallback: drawGraphic
+    });
+});
